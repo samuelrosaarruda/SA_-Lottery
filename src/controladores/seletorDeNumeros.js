d3442e8e705cd20ad5219a06ca517d230eeb0932
samuelrosaarruda <arrudarosasamuel@gmail.com>
2024-03-13 20:09:01 -0300

    Atualização do geradorDeNumeros, criação do limitador de numeros

--- src/controladores/seletorDeNumeros.js
+++ src/controladores/seletorDeNumeros.js
@@ -6,14 +6,21 @@ const geradorDeNumeros = async (req, res) => {
             return res.status(400).json({ mensagem: 'Forneça a quantidade números para gerar o sorteio' });
         }
         const numeros = [];
+
         for (let i = 1; i <= quantidadeNumero; i++) {
-let numeroSorteado = Math.floor(Math.random() * 60);
-if(numeroSorteado === 0){
-i-=1
-}
-            numeros.push(numeroSorteado)
+            let numeroSorteado = Math.floor(Math.random() * (60 - 1 + 1)) + 1;
+
+            let numeroEncontrado = numeros.includes(numeroSorteado);
+
+            if (numeroEncontrado) {
+                i -= 1;
+            }
+            else {
+                numeros.push(numeroSorteado)
+            }
         }
-        return res.json({ "Os números selecionados são": numeros });
+        return res.json({ 'Os números sorteados são': numeros });
+
     } catch (error) {
         console.log(error);
         return res.status(500).json({ mensagem: 'Erro interno no servidor' });
